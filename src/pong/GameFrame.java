df23241a25c3ebb2cf41781c7bf0e645642e7f8b
Jacek <jacek@g.pl>
2020-11-03 11:42:08 +0100

    całość

--- src/pong/GameFrame.java
+++ src/pong/GameFrame.java
@@ -1,4 +1,24 @@
 package pong;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
-public class GameFrame {
+public class GameFrame extends JFrame{
+
+    GamePanel panel;
+
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("PingPong Game");
+        this.setResizable(false);
+        this.setBackground(new Color(89, 53, 9));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+
+    }
 }
